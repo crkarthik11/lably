fd21d10dc35f08c7c44fdf7e6799dbc0241be7c8
CR <crkarthik11@gmail.com>
2021-04-28 17:09:32 +0530

    spring class 2

--- src/main/java/com/xfactor/lably/controllers/TestController.java
+++ src/main/java/com/xfactor/lably/controllers/TestController.java
@@ -1,34 +1,94 @@
 package com.xfactor.lably.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.websocket.server.PathParam;
+
+import com.xfactor.lably.entity.Lab;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import antlr.collections.List;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    // http://localhost:8080/test/hello
-    @RequestMapping("/hello")
-    public String index() {
+    ArrayList<Lab> labs = new ArrayList<>();
+
+    // @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String hello() {
         return "Greetings from XFACTOR!!!";
     }
 
-    // http://localhost:8080/test/hello/xfactor
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String index_greetings(@PathVariable String name) {
-        return "Greetings :" + name;
+    public String helloName(@PathVariable String name) {
+        return "Greetings from " + name + "!!!";
     }
 
-    // http://localhost:8080/test/hello2?id=16
     @GetMapping("/hello2")
-    @ResponseBody
-    public String getFoos(@RequestParam String id) {
-        return "ID: " + id;
+    public String helloName2(@RequestParam String name, @RequestParam String age) {
+        return "Greetings from hello2 " + name + "!!!" + age;
     }
+
+    @GetMapping("/hello3")
+    public Map<String, String> helloName3(@RequestParam String name, @RequestParam String age) {
+        Map<String, String> respoMap = new HashMap<>();
+        respoMap.put("name", name);
+        respoMap.put("age", age);
+        return respoMap;
+    }
+
+    @GetMapping("/hello4")
+    public ArrayList helloName4(@RequestParam String name, @RequestParam String age) {
+        ArrayList arrayList = new ArrayList<>();
+        arrayList.add(name);
+        arrayList.add(age);
+        return arrayList;
+    }
+
+    @GetMapping("/getLabs")
+    public ArrayList<Lab> getLabs() {
+        return labs;
+    }
+
+    @PostMapping("/addLab")
+    public Lab addLab(@RequestBody Lab lab) {
+        String name = lab.getName();
+        name = "Hello " + name;
+        lab.setName(name);
+        labs.add(lab);
+        return lab;
+    }
+
+    // // http://localhost:8080/test/hello/xfactor
+    // @GetMapping("/hello/{name}")
+    // @ResponseBody
+    // public String index_greetings(@PathVariable String name) {
+    // return "Greetings :" + name;
+    // }
+
+    // // http://localhost:8080/test/hello2?id=16
+    // @GetMapping("/hello2")
+    // @ResponseBody
+    // public String getFoos(@RequestParam String id) {
+    // return "ID: " + id;
+    // }
+
+    // @PostMapping("/employees")
+    // Employee newEmployee(@RequestBody Employee newEmployee) {
+    // return repository.save(newEmployee);
+    // }
 
 }
---
+++ src/main/java/com/xfactor/lably/entity/Lab.java
@@ -0,0 +1,45 @@
+package com.xfactor.lably.entity;
+
+public class Lab {
+
+    private String name;
+
+    private String phone;
+
+    private String address;
+
+    private String pincode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+}
