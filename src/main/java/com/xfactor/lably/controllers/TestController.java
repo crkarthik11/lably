40be4303a67cab6c0fc06f071c4db5a408418122
CR <crkarthik11@gmail.com>
2021-04-28 19:56:12 +0530

    remove unused imports

--- src/main/java/com/xfactor/lably/controllers/TestController.java
+++ src/main/java/com/xfactor/lably/controllers/TestController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.websocket.server.PathParam;
-
 import com.xfactor.lably.entity.Lab;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import antlr.collections.List;
 
 @RestController
 @RequestMapping("/test")
